79eebd70adf8f7e5a3ddbae79f9c35215435db4f
Steven Grutman <stevengrutman@stevens-mbp.myfiosgateway.com>
2023-04-10 22:49:56 -0400

    init

---
+++ Speed Square/Controllers/GameViewController.swift
@@ -0,0 +1,65 @@
+//
+//  GameViewController.swift
+//  Speed Square
+//
+//  Created by Steven Grutman on 3/18/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let view = self.view as? SKView else {
+            return
+        }
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        scene.scaleMode = .aspectFill
+        
+        view.ignoresSiblingOrder = true
+        view.showsFPS = true
+        view.showsNodeCount = true
+        view.presentScene(scene)
+        
+        
+//        view.presentScene(sceneNode)
+            
+            // Get the SKScene from the loaded GKScene
+//            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                // Copy gameplay related content over to the scene
+//                sceneNode.entities = scene.entities
+//                sceneNode.graphs = scene.graphs
+                
+                // Set the scale mode to scale to fit the window
+//                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+               
+//            }
+        
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
